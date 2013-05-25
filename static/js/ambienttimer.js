4420d57562fb3fc9af715c79ded28d428a1f5b99
Matt Phillips <mphillips@law.harvard.edu>
2013-05-25 17:31:23 -0400

    first pass at full screen feature

--- static/js/ambienttimer.js
+++ static/js/ambienttimer.js
@@ -9,7 +9,7 @@ var slide_line = svg_container.append('line')
 			.attr("y2", 300)
 			.attr("class", "slide");
 
-// Drow our grill lines
+// Draw our grill lines
 for (var i = 0; i < 21; i++) {
 	svg_container.append('line')
 				.attr("x1", i * 110)
@@ -19,8 +19,8 @@ for (var i = 0; i < 21; i++) {
 				.attr("class", "grill");
 }
 
-// Start the slide on load
 if (num_millisecs > 0) {
+    // Start the slide on load
     slide_line.transition()
     	.attr("x1", 1000)
     	.attr("y1", 0)
@@ -28,4 +28,27 @@ if (num_millisecs > 0) {
     	.attr("y2", 1000)
     	.duration(num_millisecs)
     	.ease('linear');
+    	
+    // Hide the fullscreen icon after three seconds
+    setTimeout(function(){
+        d3.select('#full_screen_container').attr("class", "hidden");
+      }, 5000);
 }
+
+
+// Fullscreen, thanks http://davidwalsh.name/fullscreen
+// Find the right method, call on correct element
+function launch_full_screen(element) {
+    if(element.requestFullScreen) {
+        element.requestFullScreen();
+    } else if(element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen();
+    }
+}
+
+d3.select('#full_screen_container').on("click", function() {
+    launch_full_screen(document.documentElement);
+    d3.select(this).attr("class", "hidden");
+    });
