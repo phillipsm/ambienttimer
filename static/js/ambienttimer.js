558e164f6ccee803b7895e64e066f70bb41f3220
Matt Phillips <mphillips@law.harvard.edu>
2013-05-28 11:29:12 -0400

    tweaked js a little

--- static/js/ambienttimer.js
+++ static/js/ambienttimer.js
@@ -1,5 +1,7 @@
-// Launch html5 fullscreen, thanks http://davidwalsh.name/fullscreen
+
 function launch_full_screen(element) {
+    // Launch html5 fullscreen
+    
     if(element.requestFullScreen) {
         element.requestFullScreen();
     } else if(element.mozRequestFullScreen) {
@@ -9,58 +11,53 @@ function launch_full_screen(element) {
     }
 }
 
-// Detect if browser supports fullscreen
 function full_screen_enabled(element) {
-
+    // Detect if browser supports fullscreen
+    
     // Test browser for fullscreen element
     var full_screen_avail = element.webkitRequestFullScreen || element.mozRequestFullScreen || element.webkitRequestFullScreen;
     
-    // If the browser is mobile, fullscreen seems spaztastic, so disable. 
-    var mobile = (/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent);
-    
-    if (mobile) {
+    // Fullscreen seems spaztastic on mobile, so don't show our fullscreen icon. 
+    if ((/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent)) {
         full_screen_avail = false;
     }
     
     return full_screen_avail;
 }
 
-if (full_screen_enabled(document.documentElement)) {
-    $('#full_screen_container').fadeIn(1000);
-}
-
-$('#full_screen_container').on('click', function() {
-    launch_full_screen(document.documentElement);
-    $(this).fadeOut(2000);
-});
-
-
-// When full screen is exited
 function on_fullscreen_exit() {
-    $('#full_screen_container').fadeIn(5000).fadeOut(5000);
+    // When full screen exists
+    
+    $('#full_screen_container').fadeIn(3000).fadeOut(5000);
 }
 
-// Listen for fullscreen exit (so we can fadein the fullscreen icon)
 if (full_screen_enabled(document.documentElement)) {
+    // Show fullscreen icon and listen for fullscreen exit (so we can fadein the fullscreen icon)
+    
+    // FadeIn and Fadeout fullscreen icon on load
+    $('#full_screen_container').fadeIn(1000).fadeOut(5000);
+    
     document.addEventListener("fullscreenchange", function () {
         if (!document.fullscreen) {
             on_fullscreen_exit()
         }
     }, false);
+    
     document.addEventListener("mozfullscreenchange", function () {
         if (!document.mozFullScreen) {
             on_fullscreen_exit()
         }
     }, false);
+    
     document.addEventListener("webkitfullscreenchange", function () {
         if (!document.webkitIsFullScreen) {
             on_fullscreen_exit()
         }
     }, false);
-
-
-    // Fadeout fullscreen icon on load
-    setTimeout(function(){
-            $('#full_screen_container').fadeOut(5000);
-        });
 }
+
+// Our clickable, fullscreen element
+$('#full_screen_container').on('click', function() {
+    launch_full_screen(document.documentElement);
+    $(this).fadeOut(2000);
+});
