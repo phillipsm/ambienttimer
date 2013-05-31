ae39e8a525ed3e24352372e7934d0131ad49f892
Matthew Phillips <labratmatt@Matthews-MacBook-Pro.local>
2013-05-30 23:29:18 -0400

    ie now gets a "please use another browser" message swapped fullscreen icon

--- static/js/ambienttimer.js
+++ static/js/ambienttimer.js
@@ -4,28 +4,48 @@
 
 // We fill in the favicon as we fill in the window
 // if we have a short timer, only swap out a couple favicons
-var favicons_to_display = [0,7,15,23];
 
-if (num_millisecs >= 3000) {
-    favicons_to_display = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-}
+// Chrome and Opera seem okay with swapping favicons
+if(/chrom(e|ium)|opera/.test(navigator.userAgent.toLowerCase())){
 
-var num_favicons = favicons_to_display.length - 1;
+    var favicons_to_display = [0,7,15,23];
 
-var swap_favicon = function() {
-    if (favicons_to_display.length > 0) {
-        $('#favicon').attr('href', '/static/img/favicon-' + favicons_to_display[0] + '.png');
-        favicons_to_display.splice(0,1);
-        setTimeout(swap_favicon, num_millisecs/num_favicons);
+    if (num_millisecs >= 3000) {
+        favicons_to_display = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+    }
+
+    var num_favicons = favicons_to_display.length - 1;
+
+    var swap_favicon = function() {
+        if (favicons_to_display.length > 0) {
+            $('#favicon').attr('href', '/static/img/favicon-' + favicons_to_display[0] + '.png');
+            favicons_to_display.splice(0,1);
+            setTimeout(swap_favicon, num_millisecs/num_favicons);
+        }
+    }
+
+    if (num_millisecs != 0) {
+        swap_favicon();
     }
 }
-
-if (num_millisecs != 0) {
-    swap_favicon();
-}
-
 //////////////////////////////////////////////////////
 // End favicon logic
+//////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////
+// Start IE management
+//////////////////////////////////////////////////////
+
+// Fullscreen (view box with 100% height and width) in IE is beyond me. Fuck it.
+
+if(/msie/.test(navigator.userAgent.toLowerCase())){
+    $('.example').remove();
+    $("#instructions").append("AmbientTimer doesn't work so well in Internet Explore. Sorry. Can you give another browser a try?");
+}
+
+
+//////////////////////////////////////////////////////
+// End IE management
 //////////////////////////////////////////////////////
 
 
@@ -62,14 +82,14 @@ function full_screen_enabled(element) {
 function on_fullscreen_exit() {
     // When full screen exists
     
-    $('#full_screen_container').fadeIn(1000).fadeOut(4000);
+    $('#full_screen_container').fadeIn(1000).fadeOut(6500);
 }
 
 if (full_screen_enabled(document.documentElement)) {
     // Show fullscreen icon and listen for fullscreen exit (so we can fadein the fullscreen icon)
     
     // FadeIn and Fadeout fullscreen icon on load
-    $('#full_screen_container').fadeIn(1000).fadeOut(4000);
+    $('#full_screen_container').fadeIn(1000).fadeOut(6500);
     
     document.addEventListener("fullscreenchange", function () {
         if (!document.fullscreen) {
