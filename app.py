ef8fff8ad5023bbfd3b40b742b0306023d9c0a74
Matthew Phillips <matt@Rhondas-iMac.local>
2014-06-10 22:58:23 -0400

    Updated license. Added support for --h--m--s syntax.

--- app.py
+++ app.py
@@ -17,24 +17,39 @@ def humans():
 @app.route('/<incoming_time>')
 def landing_with_time(incoming_time):
 
-    tokenized_times = incoming_time.split(':')
-
-    # if we get any time that contains something other than numbers or
-    # a colon, or if we get more than two colons (we want hh:mm:ss at the most)
-    if re.search('[^0-9:]+', incoming_time) or len(tokenized_times) > 3:
-        return render_template('index.html', num_millisecs=0)
-    
-    # remove any empty times from the list
-    times = filter(None, tokenized_times)
-
     num_secs = 0
 
-    if len(times) == 1:
-        num_secs = num_secs + int(times[0])
-    elif len(times) == 2:
-        num_secs = num_secs + int(times[0]) * 60 + int(times[1])
-    elif len(times) == 3:
-        num_secs = num_secs + int(times[0]) * 3600 + int(times[1]) * 60 + int(times[2])
+    if ':' in incoming_time:
+        tokenized_times = incoming_time.split(':')
+
+        # remove any empty times from the list
+        times = filter(None, tokenized_times)
+
+        if len(times) == 1:
+            num_secs = num_secs + int(times[0])
+        elif len(times) == 2:
+            num_secs = num_secs + int(times[0]) * 60 + int(times[1])
+        elif len(times) == 3:
+            num_secs = num_secs + int(times[0]) * 3600 + int(times[1]) * 60 + int(times[2])
+
+    elif re.search('[hms]+', incoming_time):
+        # We might have received the newer syntax of --h--m--s
+        if num_secs == 0:
+            secs = re.search('([0-9]*)s', incoming_time)
+            if secs:
+                num_secs += num_secs + int(secs.group(1))
+
+            mins = re.search('([0-9]*)m', incoming_time)
+            if mins:
+                num_secs += num_secs + int(mins.group(1)) * 60
+
+            hours = re.search('([0-9]*)h', incoming_time)
+            if hours:
+                num_secs += num_secs + int(hours.group(1)) * 3600
+
+
+    else:
+        return render_template('index.html', num_millisecs=0)
     
     # convert to milliseconds and return    
     return render_template('index.html', num_millisecs=num_secs * 1000)
